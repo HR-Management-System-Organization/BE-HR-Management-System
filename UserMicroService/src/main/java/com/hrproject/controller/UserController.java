c74af29099c0bd3a6cf7b917eab726a224edf66f
alicemalerzincan1996 <127924742+alicemalerzincan1996@users.noreply.github.com>
2023-11-01 17:38:25 +0300

    Update UserController.java
    Controlde eklemeler yaptim

--- UserMicroService/src/main/java/com/hrproject/controller/UserController.java
+++ UserMicroService/src/main/java/com/hrproject/controller/UserController.java
@@ -44,10 +44,10 @@ public class UserController {
 
         return ResponseEntity.ok(userService.getTotalAnnualLeave(dto));
     }
-    @GetMapping("/findallbyadmin")
-    public ResponseEntity<List<UserProfile>> findallbyadmin(String tokken){
-        return ResponseEntity.ok(userService.finduserprofilesbyadmin(tokken));
-    }
+    //@GetMapping("/findallbyadmin")
+   // public ResponseEntity<List<UserProfile>> findallbyadmin(String tokken){
+    //    return ResponseEntity.ok(userService.finduserprofilesbyadmin(tokken));
+  //  }
 
     @GetMapping("/allEmployees")
     public ResponseEntity<List<UserProfile>> getAllEmployees() {
@@ -56,4 +56,101 @@ public class UserController {
 
         return ResponseEntity.ok(employeeList);
     }
+@PostMapping("/findallbyadmin")
+    public ResponseEntity<List<UserProfile>> findallbyadmin(
+            @RequestParam(required = false) String token,
+            @RequestHeader(required = false) String authorization,
+            @RequestBody(required = false) Map<String, String> requestBody) {
+
+        if (token != null) {
+            System.out.println("Sorgu parametresinden gelen token: " + token);
+            // Sorgu parametresinden gelen token'ı işleyebilirsiniz
+        } else if (authorization != null) {
+            // İstek başlığından gelen token'ı Bearer prefix'ini ayırarak işleyebilirsiniz
+            if (authorization.startsWith("Bearer ")) {
+                String tokenWithoutBearer = authorization.substring(7); // 7, "Bearer " prefix uzunluğudur
+                System.out.println("İstek başlığından gelen token: " + tokenWithoutBearer);
+                // tokenWithoutBearer artık Bearer prefix'inden arındırılmış token'ı içerir
+                // tokenWithoutBearer değişkenini kullanarak işlemlerinizi gerçekleştirebilirsiniz
+                token=tokenWithoutBearer;
+            }
+        } else if (requestBody != null && requestBody.containsKey("token")) {
+            System.out.println("İstek gövdesinden gelen token: " + requestBody.get("token"));
+            // İstek gövdesinden gelen token'ı işleyebilirsiniz
+        } else {
+            System.out.println("Token sağlanmadı");
+        }
+
+        // Alınan token ile işlemlerinizi gerçekleştirin
+        return ResponseEntity.ok(userService.finduserprofilesbyadmin(token));
+    }@PostMapping("/findallbyadminpending")
+    public ResponseEntity<List<UserProfile>> findallbyadminpending(
+            @RequestParam(required = false) String token,
+            @RequestHeader(required = false) String authorization,
+            @RequestBody(required = false) Map<String, String> requestBody) {
+
+        if (token != null) {
+            System.out.println("Sorgu parametresinden gelen token: " + token);
+            // Sorgu parametresinden gelen token'ı işleyebilirsiniz
+        } else if (authorization != null) {
+            // İstek başlığından gelen token'ı Bearer prefix'ini ayırarak işleyebilirsiniz
+            if (authorization.startsWith("Bearer ")) {
+                String tokenWithoutBearer = authorization.substring(7); // 7, "Bearer " prefix uzunluğudur
+                System.out.println("İstek başlığından gelen token: " + tokenWithoutBearer);
+                // tokenWithoutBearer artık Bearer prefix'inden arındırılmış token'ı içerir
+                // tokenWithoutBearer değişkenini kullanarak işlemlerinizi gerçekleştirebilirsiniz
+                token=tokenWithoutBearer;
+            }
+        } else if (requestBody != null && requestBody.containsKey("token")) {
+            System.out.println("İstek gövdesinden gelen token: " + requestBody.get("token"));
+            // İstek gövdesinden gelen token'ı işleyebilirsiniz
+        } else {
+            System.out.println("Token sağlanmadı");
+        }
+
+        // Alınan token ile işlemlerinizi gerçekleştirin
+        return ResponseEntity.ok(userService.finduserprofilesbyadminpending(token));
+    }
+    @PostMapping("/activationbyadmin")
+    public void activasyonbyadmin(
+            @RequestParam Integer authorId,
+            @RequestHeader(required = false) String authorization) {
+        System.out.println(authorId);
+        String token = null;
+            if (!authorId.equals(null)){
+                if (authorization.startsWith("Bearer ")) {
+                    String tokenWithoutBearer = authorization.substring(7); // 7, "Bearer " prefix uzunluğudur
+                    System.out.println("İstek başlığından gelen token: " + tokenWithoutBearer);
+                    // tokenWithoutBearer artık Bearer prefix'inden arındırılmış token'ı içerir
+                    // tokenWithoutBearer değişkenini kullanarak işlemlerinizi gerçekleştirebilirsiniz
+                    token=tokenWithoutBearer;
+                    System.out.println(token);
+
+                }
+                Long longSayi = (long) authorId; // int'i Long'a dönüştür
+
+                userService.activitosyon(token,longSayi);
+
+
+            }
+
+
+    }
+
+    @PostMapping("/finduserbyadmin")
+    public ResponseEntity<UserProfile> findallbyadmin(
+            @RequestParam(required = false) String token,
+            @RequestHeader(required = false) String authorization){
+        if (authorization.startsWith("Bearer ")) {
+            String tokenWithoutBearer = authorization.substring(7); // 7, "Bearer " prefix uzunluğudur
+            System.out.println("İstek başlığından gelen token: " + tokenWithoutBearer);
+            // tokenWithoutBearer artık Bearer prefix'inden arındırılmış token'ı içerir
+            // tokenWithoutBearer değişkenini kullanarak işlemlerinizi gerçekleştirebilirsiniz
+            token=tokenWithoutBearer;
+        }
+        System.out.println(token);
+       return ResponseEntity.ok(userService.userProfilefindbidwithtokken(token));
+
+    }
+
 }
