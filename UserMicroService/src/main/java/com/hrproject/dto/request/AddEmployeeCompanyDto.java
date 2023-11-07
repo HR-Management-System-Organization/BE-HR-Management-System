40be4422abfe66c01eedcfb60834fe9c4a2fc5de
alicemalerzincan1996 <127924742+alicemalerzincan1996@users.noreply.github.com>
2023-11-07 09:32:31 +0300

    izn talbei ve addemployee

---
+++ UserMicroService/src/main/java/com/hrproject/dto/request/AddEmployeeCompanyDto.java
@@ -0,0 +1,53 @@
+package com.hrproject.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AddEmployeeCompanyDto {
+
+    
+
+    @NotEmpty(message = "Name cannot be empty")
+    @Size(min = 3, max = 20, message = "Name must be between 3 and 20 characters.")
+    private String name;
+
+    @NotEmpty(message = "Surname cannot be empty")
+    @Size(min = 3, max = 20, message = "Surname must be between 3 and 20 characters.")
+    private String surname;
+
+    @NotEmpty(message = "Username cannot be empty")
+    @Size(min = 3, max = 20, message = "Username must be between 3 and 20 characters.")
+    private String username;
+
+    @Email(message = "Please enter a valid email address.")
+    private String personalEmail;
+
+    @NotEmpty
+    @Pattern(message = "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one digit, and one special character.",
+            regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=*!])(?=\\S+$).{8,32}$")
+    private String password;
+
+    @NotEmpty(message = "Phone cannot be empty" )
+    private String phone;
+
+    @NotEmpty(message = "Address cannot be empty" )
+    private String address;
+
+    private String info;
+
+    private String avatar;
+
+    @NotEmpty(message = "Birthday cannot be empty" )
+    private String birthday;
+}
---
+++ UserMicroService/src/main/java/com/hrproject/repository/enums/EIzinTur.java
@@ -0,0 +1,5 @@
+package com.hrproject.repository.enums;
+
+public enum EIzinTur {
+    Yillikizin,Babalikizini,Annelikizini;
+}
